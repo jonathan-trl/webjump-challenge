833986d8e2844fb1d92f9e3be5f932baa0f26a16
Jonathan Carvalho <jonathan.carvalho1807@gmail.com>
2024-03-26 11:11:09 -0300

    feat: Created product type filters

---
+++ public/src/config/filters.ts
@@ -0,0 +1,20 @@
+import { Filter } from '../types/filters'
+
+export const filterTypes: Filter[] = [
+  {
+    id: 1,
+    label: 'Corrida',
+  },
+  {
+    id: 2,
+    label: 'Caminhada',
+  },
+  {
+    id: 3,
+    label: 'Casual',
+  },
+  {
+    id: 4,
+    label: 'Social',
+  },
+]
